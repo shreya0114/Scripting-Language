e159d130db8edb79b8661cf4c867eef669ec70d3
shreya0114 <shreyaluitel4@gmail.com>
2025-09-11 17:31:06 +0545

    Update check Divisible.php

--- check Divisible.php
+++ check Divisible.php
@@ -1,12 +1,43 @@
 <?php
-function isDivisibleBy5($num) {
-    return $num % 5 == 0;
-}
-echo "Enter a number: ";
-$number = trim(fgets(STDIN));
-if (isDivisibleBy5($number)) {
-    echo "$number is divisible by 5.";
-} else {
-    echo "$number is not divisible by 5.";
+$result = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $input = $_POST['number'];
+
+    if (empty($input)) {
+        $result = "<span style='color:red;'>Please enter a number.</span>";
+    } elseif (!is_numeric($input) || intval($input) != $input) {
+        $result = "<span style='color:red;'>Please enter a valid integer.</span>";
+    } else {
+        $number = intval($input);
+
+        function isDivisibleByFive($num) {
+            return $num % 5 === 0;
+        }
+
+        if (isDivisibleByFive($number)) {
+            $result = "<span style='color:green;'>$number is divisible by 5.</span>";
+        } else {
+            $result = "<span style='color:red;'>$number is NOT divisible by 5.</span>";
+        }
+    }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Divisibility by 5 Checker</title>
+</head>
+<body>
+    <h2>Check if a Number is Divisible by 5</h2>
+
+    <form method="post">
+        <label>Enter an integer:</label>
+        <input type="text" name="number" value="<?php echo isset($_POST['number']) ? htmlspecialchars($_POST['number']) : ''; ?>" required>
+        <input type="submit" value="Check">
+    </form>
+
+    <p><?php echo $result; ?></p>
+</body>
+</html>
